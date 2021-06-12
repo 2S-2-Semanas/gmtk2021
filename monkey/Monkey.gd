91f3bdd73959008eccdbb86bef35f42e18355967
Elena <elenam19396@gmail.com>
2021-06-13 00:17:26 +0200

    added current monkey liana grabbed

--- monkey/Monkey.gd
+++ monkey/Monkey.gd
@@ -8,6 +8,8 @@ const IMPULSE_AFTER_RELEASE := Vector2(300, -1000)
 signal liana_grabed
 signal liana_released
 
+var _current_monkey_liana_grabbed:= false
+
 var _left_pin_joint: PinJoint2D
 var _right_pin_joint: PinJoint2D
 
@@ -34,10 +36,10 @@ func _physics_process(_delta):
 		_direction = 0
 		
 	if (Input.is_action_just_released("grab")):
-		if(MonkeyGlobal._liana_grabbed):
+		if(MonkeyGlobal._liana_grabbed and _current_monkey_liana_grabbed):
 			apply_impulse(Vector2.ZERO, Vector2(IMPULSE_AFTER_RELEASE.x * _direction, IMPULSE_AFTER_RELEASE.y))
-		_release_liana()
-	if (MonkeyGlobal._liana_grabbed):
+		_release_lianas()
+	if (MonkeyGlobal._liana_grabbed and _current_monkey_liana_grabbed):
 		var resulting_impulse = IMPULSE
 		resulting_impulse.x = IMPULSE.x * _direction *_delta
 		resulting_impulse.y = IMPULSE.y * _delta
@@ -74,6 +76,7 @@ func _on_RightArmArea2D_body_entered(body):
 	elif (Input.is_action_pressed("grab") and !MonkeyGlobal._liana_grabbed):
 		_grab_right_hand(body)
 		emit_signal("liana_grabed")
+		_current_monkey_liana_grabbed = true
 
 
 func _on_LeftArmArea2D_body_entered(body):
@@ -88,6 +91,7 @@ func _on_LeftArmArea2D_body_entered(body):
 	elif (Input.is_action_pressed("grab") and !MonkeyGlobal._liana_grabbed):
 		_grab_left_hand(body)
 		emit_signal("liana_grabed")
+		_current_monkey_liana_grabbed = true
 
 
 func _grab_right_hand(body):
@@ -113,6 +117,8 @@ func _release_right_hand():
 		
 	_right_pin_joint.node_b = get_path()
 	emit_signal("liana_released")
+	_current_monkey_liana_grabbed = false
+	
 
 
 func _release_left_hand():
@@ -121,3 +127,4 @@ func _release_left_hand():
 		
 	_left_pin_joint.node_b = get_path()
 	emit_signal("liana_released")
+	_current_monkey_liana_grabbed = false
