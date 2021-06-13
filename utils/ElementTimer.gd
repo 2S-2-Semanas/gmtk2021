d10d5a2d82f51a673399067ec93d6b9b582f8fae
Elena <elenam19396@gmail.com>
2021-06-13 13:18:30 +0200

    liana random

---
+++ utils/ElementTimer.gd
@@ -0,0 +1,17 @@
+extends Timer
+
+signal elements_generated
+
+const INITIAL_WAIT_TIME: float = 1.0
+const MAX_COUNTER: int = 2
+
+var level: float = 2
+var counter: int = MAX_COUNTER
+
+func connect_rock(rock):
+	var error = connect("timeout", rock, "_on_RockTimer_timeout")
+	if error != null && error > 0:
+		print(error)
+
+func _on_LianaTimer_timeout() -> void:
+	emit_signal("elements_generated")
