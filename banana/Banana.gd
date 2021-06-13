a8292bb941c1cf02c96480cdb4211ef7c40f6ac6
Elena <elenam19396@gmail.com>
2021-06-13 19:09:51 +0200

    random bananas

--- banana/Banana.gd
+++ banana/Banana.gd
@@ -1,4 +1,5 @@
 extends Node2D
+class_name Banana
 
 onready var banan_area: Area2D = $BananaArea 
 
--- utils/ElementTimer.gd
+++ utils/ElementTimer.gd
@@ -9,9 +9,10 @@ var level: float = 2
 var counter: int = MAX_COUNTER
 
 func connect_rock(rock):
-	var error = connect("timeout", rock, "_on_RockTimer_timeout")
+	var error = connect("timeout", rock, "_on_LianaTimer_timeout")
 	if error != null && error > 0:
 		print(error)
 
 func _on_LianaTimer_timeout() -> void:
-	emit_signal("elements_generated")
+	emit_signal("elements_generated", "res://liana/Liana.tscn")
+	emit_signal("elements_generated", "res://banana/Banana.tscn")
